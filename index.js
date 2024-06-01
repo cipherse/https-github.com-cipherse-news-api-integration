3fa9b58e8c3954e85bb921fe348f971acc04ab78
cipherse <155891470+cipherse@users.noreply.github.com>
2024-06-01 18:19:35 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NewsHeadlines from './NewsHeadlines';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <NewsHeadlines />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
